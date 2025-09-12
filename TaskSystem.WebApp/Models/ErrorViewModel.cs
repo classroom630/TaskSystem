1b93321787916d60dd28cc8de6f5d1a8525b2138
Zeshan <Zeshan@DESKTOP-VKIJ4UV>
2025-09-12 22:08:38 +0500

    Add project files.

---
+++ TaskSystem.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskSystem.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
